9efb0c0db4d748dd3c67a57cc148bb850a4e964f
Godwin Olletey <godwinolle@gmail.com>
2020-12-26 21:04:31 -0500

    adding books functionality as well as forms with handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Shelf from './components/shelf/shelf'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Book Shelf</h1>
+      <Shelf />
     </div>
   );
 }
---
+++ src/components/shelf/shelf.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+
+let books = [
+    {
+        title: 'Relentless',
+        author: 'Mark Twain',
+        status: 'Not Read',
+        date: '2020-12-03'
+    },
+    {
+        title: 'Apple',
+        author: 'Tim Cook',
+        status: 'Reading',
+        date: '2020-12-23'
+    }
+]
+
+const Shelf = () => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [status, setStatus] = useState("Read");
+    const [date, setDate] = useState("N/A");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(title === "" || author === ""){
+            alert("Woah you didn't type anything in")
+        } else{
+            console.log(title);
+            console.log(author);
+            console.log(status)
+            console.log(date);
+
+            books.push(
+                {
+                    title: title,
+                    author: author,
+                    status: status,
+                    date: date
+                }
+            )
+        }
+    }
+
+    return(
+        <div>
+            <form onSubmit={ handleSubmit }>
+                <table>
+                    <thead>
+                        <tr className="heading">
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Status</th>
+                            <th>Date Finished</th>
+                            <th>Add/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input type="text" value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                />
+                            </td>
+                            <td>
+                                <input type="text" value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                                />
+                            </td>
+                            <td>
+                                <select value={status} onChange={(e) => setStatus(e.target.value)}>
+                                    <option value="Read" defaultValue>Read</option>
+                                    <option value="Reading">Reading</option>
+                                    <option value="Not Read">Not Read</option>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="date" value={date}
+                                onChange={(e) => setDate(e.target.value)}
+                                />
+                            </td>
+                            <td className="add">
+                                <button type="submit">
+                                    Add Book
+                                </button>
+                            </td>
+                        </tr>
+                        {
+                            books.map((book,i) => {
+                                return(
+                                    <tr key={i}>
+                                        <td>{book.title}</td>
+                                        <td>{book.author}</td>
+                                        <td>{book.status}</td>
+                                        <td>{book.date}</td>
+                                        <td className="delete">X</td>
+                                        </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table> 
+            </form>
+        </div>
+    )
+}
+
+export default Shelf;
